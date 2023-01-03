1455c59a5bc1a019add0dd6d21489e042976fe9d
Cloner ops <clonerops@gmail.com>
2023-01-03 11:49:12 +0330

    create Customer Call Registration

--- src/app/pages/dashboard/CustomerCallRegistration.tsx
+++ src/app/pages/dashboard/CustomerCallRegistration.tsx
@@ -1,11 +1,18 @@
 const CustomerCallRegistration = () => {
   return (
-    <div className='card'>
-      <section className='flex justify-start items-center p-5'>
-        <label className="px-2">شماره تماس گیرنده:</label>
-        <h4 className="px-2 font-VazirBold">09197563415</h4>
-      </section>
-    </div>
+    <>
+      <div className='card'>
+        {/* Customer Call Number */}
+        <section className='flex justify-start items-center p-5'>
+          <label className='px-2'>شماره تماس گیرنده:</label>
+          <h4 className='px-2 font-VazirBold'>09197563415</h4>
+        </section>
+
+        {/* Call Registration Action Begin */}
+        
+        {/* Call Registration Action End */}
+      </div>
+    </>
   )
 }
 
